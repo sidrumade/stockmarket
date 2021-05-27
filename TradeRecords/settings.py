83644b551fa4a660e7e468cc706c43ef25e51ca4
siddhesh rumade <siddhesh.rumade.5@gmail.com>
2021-05-27 16:14:10 +0530

    Host configured

--- TradeRecords/settings.py
+++ TradeRecords/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uy3d$vxp$x)9z$44_x!25_759^)j!u&e%dnro0cq(nt2_ww@pu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','django-trading-helper.herokuapp']
+ALLOWED_HOSTS = ['127.0.0.1','django-trading-helper.herokuapp.com']
 
 # Application definition
 
